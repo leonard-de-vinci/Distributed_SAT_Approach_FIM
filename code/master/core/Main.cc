76b9ea1fee42e43b7bb5cd04702cbef9777f05c2
Julien MARTIN-PRIN <julienmartinprin@gmail.com>
2021-06-18 13:53:28 +0200

    Added kafka code

--- code/master/core/Main.cc
+++ code/master/core/Main.cc
@@ -50,6 +50,18 @@ string convertToString(char* a){
     return s;
 }
 
+static void on_delivery(rd_kafka_t *rk, const rd_kafka_message_t *rkmessage, void *opaque){
+    if (rkmessage->err)
+        fprintf(stderr, "%% Message delivery failed: %s\n", rd_kafka_message_errstr(rkmessage));
+}
+
+void init_rd_kafka(){
+  rd_kafka_conf_t *conf = rd_kafka_conf_new();
+  rd_kafka_conf_set_dr_msg_cb(conf, on_delivery);
+
+  // initialization omitted
+}
+
 // Main:
 
 
@@ -186,37 +198,57 @@ int main(int argc, char** argv)
 
         //Kafka
 
-        // string brokers = "";
-        // string topic_name = "";
-        // int partition_value = -1;
+        char hostname[128];
+        char errstr[512];
 
-        // boost::program_options::options_description options("Options");
-        // options.add_options()
-        //     ("help,h", "produce this help message")
-        //     ("brokers,b", boost::program_options::value<string>(&brokers)->required(), "the kafka borker list")
-        //     ("topic,t", boost::program_options::value<string>(&topic_name)->required(), "the topic in which to write to")
-        //     ("partition,p", boost::program_options::value<int>(&partition_value), "the partition to write into (unassigned if not provided)")
-        //     ;
+        rd_kafka_conf_t *conf = rd_kafka_conf_new();
+
+        if (gethostname(hostname, sizeof(hostname))){
+            fprintf(stderr, "%% Failed to lookup hostname\n");
+            exit(1);
+        }
+
+        if (rd_kafka_conf_set(conf, "client.id", hostname, errstr, sizeof(errstr)) != RD_KAFKA_CONF_OK){
+            fprintf(stderr, "%% %s\n", errstr);
+            exit(1);
+        }
+
+        if (rd_kafka_conf_set(conf, "bootstrap.servers", "host1:9092,host2:9092", errstr, sizeof(errstr)) != RD_KAFKA_CONF_OK){
+            fprintf(stderr, "%% %s\n", errstr);
+            exit(1);
+        }
+
+        rd_kafka_topic_conf_t *topic_conf = rd_kafka_topic_conf_new();
+
+        if (rd_kafka_topic_conf_set(topic_conf, "acks", "all", errstr, sizeof(errstr)) != RD_KAFKA_CONF_OK){
+            fprintf(stderr, "%% %s\n", errstr);
+            exit(1);
+        }
+
+        /* Create Kafka producer handle */
+        rd_kafka_t *rk;
+        if (!(rk = rd_kafka_new(RD_KAFKA_PRODUCER, conf, errstr, sizeof(errstr)))) {
+            fprintf(stderr, "%% Failed to create new producer: %s\n", errstr);
+            exit(1);
+        }
+
+        rd_kafka_topic_t *rkt = rd_kafka_topic_new(rk, topic, topic_conf);
+
+        if (rd_kafka_produce(rkt, RD_KAFKA_PARTITION_UA, RD_KAFKA_MSG_F_COPY, payload, payload_len, key, key_len, NULL) == -1){
+            fprintf(stderr, "%% Failed to produce to topic %s: %s\n", topic, rd_kafka_err2str(rd_kafka_errno2err(errno)));
+        }
+
         
-        // boost::program_options::variables_map vm;
-
-        // try{
-        //     boost::program_options::store(boost::command_line_parser(argc, argv).options(options).run(), vm);
-        //     boost::program_options::notify(vm);
-        // }
-        // catch(std::exception& e){
-        //     cout << "Error parsing options: "
-        // }
         
-        Configuration config{
-            {"metadata.broker.list", "localhost:9092"}
-        };
+        // Configuration config{
+        //     {"metadata.broker.list", "localhost:29092"}
+        // };
 
-        Producer producer(config);
+        // Producer producer(config);
 
-        string message = "Hello there";
-        producer.produce(MessageBuilder("mytopic").partition(0).payload(message));
-        producer.flush();
+        // string message = "Hello there";
+        // producer.produce(MessageBuilder("mytopic").partition(0).payload(message));
+        // producer.flush();
 
         vec<Lit> dummy;
 		lbool ret;
