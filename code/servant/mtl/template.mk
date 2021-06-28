8252736b40a40f99562affc7f4a03df93a77d767
Julien MARTIN-PRIN <julienmartinprin@gmail.com>
2021-06-28 14:41:46 +0200

    Added kafka link to the makefile

--- code/servant/mtl/template.mk
+++ code/servant/mtl/template.mk
@@ -5,28 +5,48 @@
 ##        "make d"  for a debug version (no optimizations).
 ##        "make"    for the standard version (optimized, but with debug information and assertions active)
 
+##MROOT: the parent directory
+##PWD: gives the current directory
+##EXEC: gives the file names inside the actual directory if there is nothing in the variable
 MROOT      = ../
 PWD        = $(shell pwd)
 EXEC      ?= $(notdir $(PWD))
 
-CSRCS      = $(wildcard $(PWD)/*.cc) 
+##DEPDIR set in the parent file (ex. Makefile in /core)
+
+##CSRCS: gets all the .cc in the actual directory
+##DSRCS:
+##CHDRS: gets all the .h in the actual directory
+##COBJS:
+CSRCS      = $(wildcard $(PWD)/*.cc)
 DSRCS      = $(foreach dir, $(DEPDIR), $(filter-out $(MROOT)/$(dir)/Main.cc, $(wildcard $(MROOT)/$(dir)/*.cc)))
 CHDRS      = $(wildcard $(PWD)/*.h)
 COBJS      = $(CSRCS:.cc=.o) $(DSRCS:.cc=.o)
 
+##PCOBJS: adds the suffix p to all COBJS
+##DCOBJS: adds the suffix d to all COBJS
+##RCOBJS: adds the suffix r to all COBJS
 PCOBJS     = $(addsuffix p,  $(COBJS))
 DCOBJS     = $(addsuffix d,  $(COBJS))
 RCOBJS     = $(addsuffix r,  $(COBJS))
 
 
+##CXX: sets g++ to v8 if there is nothing set
+##CFLAGS:
+##LFLAGS:
 CXX       ?= g++-8
-CFLAGS    ?= -Wall -Wno-parentheses 
+CFLAGS    ?= -Wall -Wno-parentheses
 LFLAGS    ?= -Wall
 
+##COPTIMIZE: sets COPTIMIZE flag to -03 if it's empty
 COPTIMIZE ?= -O3
 
+##CFLAGS:
+##LFLAGS:
 CFLAGS    += -I$(MROOT) -D __STDC_LIMIT_MACROS -D __STDC_FORMAT_MACROS
 LFLAGS    += -lz
+
+KAFKA = -I/usr/local/include -L/usr/local/lib -lrdkafka -lm
 
 .PHONY : s p d r rs clean 
 
@@ -69,21 +89,29 @@ lib$(LIB)_release.a:	$(filter-out */Main.or, $(RCOBJS))
 
 ## Build rule
 %.o %.op %.od %.or:	%.cc
+##echo message
+##compiler (g++) flags () kafka flags (-lrdkafka -lcppkafka) -c -o $@ $<
 	@echo Compiling: $(subst $(MROOT)/,,$@)
-	@$(CXX) $(CFLAGS) -c -o $@ $<
+	@$(CXX) $(CFLAGS) -c -o $@ $< $(KAFKA)
 
 ## Linking rules (standard/profile/debug/release)
 $(EXEC) $(EXEC)_profile $(EXEC)_debug $(EXEC)_release $(EXEC)_static:
+##echo message
+##compiler (g++) $^ flags () kafka flags (-lrdkafka -lcppkafka) -o $@
 	@echo Linking: "$@ ( $(foreach f,$^,$(subst $(MROOT)/,,$f)) )"
-	@$(CXX) $^ $(LFLAGS) -o $@
+	@$(CXX) $^ $(LFLAGS) -o $@ $(KAFKA)
 
 ## Library rules (standard/profile/debug/release)
 lib$(LIB)_standard.a lib$(LIB)_profile.a lib$(LIB)_release.a lib$(LIB)_debug.a:
+##echo message
+##AR? -rcsv $@ $^
 	@echo Making library: "$@ ( $(foreach f,$^,$(subst $(MROOT)/,,$f)) )"
 	@$(AR) -rcsv $@ $^
 
 ## Library Soft Link rule:
 libs libp libd libr:
+##echo message
+##ln -sf $^ lib
 	@echo "Making Soft Link: $^ -> lib$(LIB).a"
 	@ln -sf $^ lib$(LIB).a
 
@@ -95,7 +123,7 @@ clean:
 ## Make dependencies
 depend.mk: $(CSRCS) $(CHDRS)
 	@echo Making dependencies
-	@$(CXX) $(CFLAGS) -I$(MROOT) \
+	@$(CXX) $(CFLAGS) -I$(MROOT) $(KAFKA) \
 	   $(CSRCS) -MM | sed 's|\(.*\):|$(PWD)/\1 $(PWD)/\1r $(PWD)/\1d $(PWD)/\1p:|' > depend.mk
 	@for dir in $(DEPDIR); do \
 	      if [ -r $(MROOT)/$${dir}/depend.mk ]; then \
@@ -106,3 +134,13 @@ depend.mk: $(CSRCS) $(CHDRS)
 
 -include $(MROOT)/mtl/config.mk
 -include depend.mk
+
+##g++-10 -Wall -Wno-parentheses -I../ -D __STDC_LIMIT_MACROS -D __STDC_FORMAT_MACROS \
+	-O3 -g -D DEBUG -fopenmp -lrdkafka -lcppkafka -c -o path_to_Main.o path_to_Main.cc
+## g++-10: compiler
+## -Wall: all warnings
+## -Wno-parentheses: 
+## -I../: adds the directory ../ to the list of directory to be searched for header files
+## -D:
+## -O3: Optimizes the code (compilation takes more time)
+## -fopenmp: allows pragma to work (multi threading) WARNING to -pthread
