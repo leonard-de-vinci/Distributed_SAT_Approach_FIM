888eee52edd269134a728a9fa08a12461c3a366b
Julien MARTIN-PRIN <julienmartinprin@gmail.com>
2021-07-31 15:49:32 +0200

    Removed shell script

--- code/master/start.sh
+++
@@ -1,4 +0,0 @@
-cp mongo.config core/mongo.config
-cp kafka.config core/librdkafka.config
-
-./core/master -minSupport=$MINSUPPORT -nsolvers=$NSOLVERS -reset=$RESET $DATASET
--- code/servant/start.sh
+++
@@ -1,4 +0,0 @@
-cp mongo.config core/mongo.config
-cp kafka.config core/librdkafka.config
-
-./core/slave -minSupport=$MINSUPPORT -ncores=$NCORES
