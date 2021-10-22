86e36e39367efe8eb3131585dba666cbba011277
Julien MARTIN-PRIN <julienmartinprin@gmail.com>
2021-10-22 17:55:56 +0200

    Added start and stop scripts

---
+++ start-all.sh
@@ -0,0 +1,2 @@
+kubectl scale deployment master --replicas 1
+kubectl scale deployment slave --replicas 1
---
+++ stop-all.sh
@@ -0,0 +1,2 @@
+kubectl scale deployment slave --replicas 0
+kubectl scale deployment master --replicas 0
