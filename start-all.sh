e124e31bdcddb1636f46623fa0503d0095301eda
Julien MARTIN-PRIN <julienmartinprin@gmail.com>
2021-10-25 08:34:47 +0200

    Removed unused scripts

--- start-all.sh
+++
@@ -1,2 +0,0 @@
-kubectl scale deployment master --replicas 1
-kubectl scale deployment slave --replicas 1
--- stop-all.sh
+++
@@ -1,2 +0,0 @@
-kubectl scale deployment slave --replicas 0
-kubectl scale deployment master --replicas 0
