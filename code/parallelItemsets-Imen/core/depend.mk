2c1922d0a51b259c57bbb8abf7f2af63e97e8c31
Julien MARTIN-PRIN <julienmartinprin@gmail.com>
2021-10-28 15:14:47 +0200

    Fixed some bugs

--- code/parallelItemsets-Imen/core/depend.mk
+++ code/parallelItemsets-Imen/core/depend.mk
@@ -1,5 +1,5 @@
-/Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/Cooperation.o /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/Cooperation.or /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/Cooperation.od /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/Cooperation.op: \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/Cooperation.cc \
+/Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/Cooperation.o /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/Cooperation.or /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/Cooperation.od /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/Cooperation.op: \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/Cooperation.cc \
  ../core/Cooperation.h ../core/SolverTypes.h ../core/../mtl/IntTypes.h \
  ../core/../mtl/Alg.h ../core/../mtl/Vec.h ../core/../mtl/IntTypes.h \
  ../core/../mtl/XAlloc.h ../core/../mtl/Vec.h ../core/../mtl/Map.h \
@@ -7,32 +7,32 @@
  ../core/../utils/Options.h ../core/../utils/../mtl/IntTypes.h \
  ../core/../utils/../mtl/Vec.h ../core/../utils/ParseUtils.h \
  ../mtl/Sort.h ../mtl/Vec.h
-/Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/Main.o /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/Main.or /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/Main.od /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/Main.op: \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/Main.cc \
+/Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/Main.o /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/Main.or /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/Main.od /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/Main.op: \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/Main.cc \
  /usr/local/include/omp.h \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/../utils/System.h \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/../utils/../mtl/IntTypes.h \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/../utils/ParseUtils.h \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/../utils/Options.h \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/../utils/../mtl/Vec.h \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/../utils/../mtl/IntTypes.h \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/../utils/../mtl/XAlloc.h \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/../utils/ParseUtils.h \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/Dimacs.h \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/Cooperation.h \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/SolverTypes.h \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/../mtl/IntTypes.h \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/../mtl/Alg.h \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/../mtl/Vec.h \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/../mtl/Vec.h \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/../mtl/Map.h \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/../mtl/IntTypes.h \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/../mtl/Alloc.h \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/../mtl/XAlloc.h \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/Solver.h \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/../mtl/Heap.h
-/Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/Solver.o /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/Solver.or /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/Solver.od /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/Solver.op: \
- /Users/Flexiboy/Desktop/Projects/Research/parrallelItemsets-Imen/core/Solver.cc \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/../utils/System.h \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/../utils/../mtl/IntTypes.h \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/../utils/ParseUtils.h \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/../utils/Options.h \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/../utils/../mtl/Vec.h \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/../utils/../mtl/IntTypes.h \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/../utils/../mtl/XAlloc.h \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/../utils/ParseUtils.h \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/Dimacs.h \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/Cooperation.h \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/SolverTypes.h \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/../mtl/IntTypes.h \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/../mtl/Alg.h \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/../mtl/Vec.h \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/../mtl/Vec.h \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/../mtl/Map.h \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/../mtl/IntTypes.h \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/../mtl/Alloc.h \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/../mtl/XAlloc.h \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/Solver.h \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/../mtl/Heap.h
+/Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/Solver.o /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/Solver.or /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/Solver.od /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/Solver.op: \
+ /Users/Flexiboy/Desktop/Projects/Distributed_SAT_Approach_FIM/code/parallelItemsets-Imen/core/Solver.cc \
  /usr/local/include/omp.h ../mtl/Sort.h ../mtl/Vec.h ../mtl/IntTypes.h \
  ../mtl/XAlloc.h ../core/Solver.h ../core/../mtl/Vec.h \
  ../core/../mtl/Heap.h ../core/../mtl/Vec.h ../core/../mtl/Alg.h \
